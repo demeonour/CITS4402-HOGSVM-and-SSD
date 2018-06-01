d272c390d5dea359c75086942edd35dd9c3ee2ab
demeonour <mitchpoo13@gmail.com>
2018-06-01 13:34:33 +0800

    HOG Changes

--- hogsvm.py
+++ hogsvm.py
@@ -16,10 +16,9 @@ args = vars(ap.parse_args())
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# loop over the image paths
+# loop over the image path
 for imagePath in paths.list_images(args["images"]):
-	# load the image and resize it to (1) reduce detection time
-	# and (2) improve detection accuracy
+	# loads the image and resizes it 
 	image = cv2.imread(imagePath)
 	image = imutils.resize(image, width=min(400, image.shape[1]))
 	orig = image.copy()
@@ -33,15 +32,13 @@ for imagePath in paths.list_images(args["images"]):
 	# draw the original bounding boxes
 	for (x, y, w, h) in rects:
 		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
+	# apply non-maxima suppression to the bounding boxes
 	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
 	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 	# draw the final bounding boxes
 	for (x1, y1, x2, y2) in pick:
 		cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-	# show some information on the number of bounding boxes
+	# show the information on the number of bounding boxes before/after NMS
 	filename = imagePath[imagePath.rfind("/") + 1:]
 	print("[INFO] {}: {} original boxes, {} after suppression".format(
 		filename, len(rects), len(pick)))
